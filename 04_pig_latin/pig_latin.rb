0e6142e2b18e48a21109bf3430b2165a041c4797
Arsirion <arsirion@gmail.com>
2016-10-09 11:45:20 -0500

    first two tests passed on 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,14 @@
 #write your code here
+def translate (english)
+	vowels = "aeoiu"
+	firstletter = english[0]
+	remainder = english[1, english.length]
+	phonemes = ['sch', 'ch', 'qu']
+	if vowels.include?(firstletter)
+		piglatin = english + 'ay'
+		return piglatin
+	else 
+		piglatin = remainder + firstletter + 'ay'
+		return piglatin
+	end
+end
