793aab5524d59cd03661be705ad4a70ea085a79a
Arsirion <arsirion@gmail.com>
2016-10-16 13:01:53 -0500

    all but one test

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,14 +1,35 @@
-#write your code here
-def translate (english)
-	vowels = "aeoiu"
-	firstletter = english[0]
-	remainder = english[1, english.length]
-	phonemes = ['sch', 'ch', 'qu']
-	if vowels.include?(firstletter)
-		piglatin = english + 'ay'
-		return piglatin
-	else 
-		piglatin = remainder + firstletter + 'ay'
-		return piglatin
-	end
+def translate(word)
+	engphrase = word.split(' ')
+	# puts engphrase
+	translated = engphrase.collect{|english|
+	vowels = 'aeiou'
+	# puts english
+
+	if !(vowels.include?(english[0]))
+		if !(vowels.include?(english[1]))
+			if !(vowels.include?(english[2]))
+				piglatin = english[3..english.length]
+				piglatin << english[0..2] << 'ay'
+				return piglatin
+			end
+			piglatin = english[2..english.length]
+			piglatin << english[0..1] << 'ay'
+			return piglatin
+		end
+		piglatin = english[1..english.length]
+		piglatin << english[0] << 'ay'
+		# return piglatin
+	elsif english.include?('qu')
+		piglatin = english.split('qu').to_ary.reverse.join << 'qu' << 'ay'
+		# return piglatin
+	else
+		piglatin = english << 'ay'
+		# return piglatin
+	end}
+	# puts translated
+	return translated.join(' ')
 end
+
+# puts translate('two words')
+# puts translate('cherry')
+# puts translate('school')
