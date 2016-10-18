8441e51b5ea191bff0df5fa88832834c147b3d47
Arsirion <arsirion@gmail.com>
2016-10-17 20:26:25 -0500

    Finished last two excercises

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,25 @@
+
 class Book
-# write your code here
+ def initialize()
+   @title = title
+ end
+ def title=(title)
+   
+   title = title.split.to_ary.map!{|word|
+       
+       if(word == "and")||(word == "or") ||(word == "the") ||(word == "of") || (word == "if") ||(word == "in") || (word =="a") || (word =="an")
+       word = word
+       else
+       word = word.capitalize
+       end}
+      title[0] = title[0].capitalize
+     
+       @title = title.join(' ')
+     
+ end
+ def title
+   @title
+   
+ end
+ 
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,20 @@
 class Timer
-  #write your code here
+
+	def initialize()
+		@seconds = 0
+	end
+# returns object variable of seconds when timer.seconds is called
+	def seconds
+		@seconds
+	end
+# sets seconds for timer when timer.seconds = INT or timer.seconds=(INT) is called
+	def seconds=(seconds)
+		@seconds = seconds
+	end
+# returns time as a string when timer.time_string is called
+	def time_string 
+		time = Time.gm(2000,1,1) + @seconds
+		@time_string = "#{time.strftime("%H:%M:%S")}"
+	end
+
 end
